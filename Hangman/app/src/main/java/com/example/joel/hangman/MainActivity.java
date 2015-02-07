20039e92d273b725a07321b917aeacde5ac9e45a
Joel Braun <joelbraun@outlook.com>
2015-02-06 17:20:15 -0800

    Android app works
    yay

---
+++ Hangman/app/src/main/java/com/example/joel/hangman/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.joel.hangman;
+
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.util.Log;
+import android.widget.EditText;
+import java.util.List;
+import java.util.ArrayList;
+import android.media.MediaPlayer;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    protected String wordstring(String s){
+        String visible = "";
+        for (int i = 0; i < s.length(); ++i) {
+            visible += "_ ";
+        }
+        return visible;
+    }
+
+    protected int convert(String s) {
+        switch (s) {
+            case "5":
+                return 5;
+            case "4":
+                return 4;
+            case "3":
+                return 3;
+            case "2":
+                return 2;
+            case "1":
+                return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button play = (Button) this.findViewById(R.id.play);
+        final EditText input = (EditText)this.findViewById(R.id.editText);
+        final TextView wdview = (TextView)this.findViewById(R.id.WordView);
+        final TextView statusdisplay = (TextView)this.findViewById(R.id.Status);
+        final String word = "mussolini";
+        final String visible1 = wordstring(word);
+        wdview.setText(wordstring(word));
+        final ImageView marmot = (ImageView)this.findViewById(R.id.imageView);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.anchorsaweigh);
+        final TextView tv = (TextView) this.findViewById(R.id.Remaining);
+        tv.setText("5");
+        final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.piazza);
+
+        play.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 int remainingGuesses;
+                 String wrongGuesses;
+
+                 String visible;
+                 List<String> l = new ArrayList<String>();
+
+                    remainingGuesses = convert(tv.getText().toString());
+                    wrongGuesses = "";
+
+
+                      visible = wdview.getText().toString();
+
+
+                    String status = "You have "+remainingGuesses+" remaining";
+                    String wrong = "Wrong guesses so far: "+wrongGuesses;
+
+
+                            String text = input.getText().toString();
+                            Boolean flag = true;
+
+                            for (String s : l){
+                                if (text.equals(s)){
+                                    flag = false;
+                                }
+
+                            }
+
+
+                            if(text.length()  == 1 && text.matches("[a-z]") && flag) {
+
+                                boolean guessFound = false;
+
+                                l.add(text);
+
+                                for(int i = 0; i < word.length(); ++i) {
+                                    if(text.charAt(0) == word.charAt(i)) {
+                                        guessFound = true;
+
+                                        String newVisible = "";
+                                        for(int j = 0; j < visible.length(); ++j) {
+                                            if(j == (i*2)) {
+                                                newVisible += word.charAt(i);
+                                            }
+                                            else {
+                                                newVisible += visible.charAt(j);
+                                            }
+                                        }
+                                        visible = newVisible;
+                                        wdview.setText(visible);
+                                    }
+                                }
+
+                                if(!guessFound) {
+
+                                    if(--remainingGuesses > 0) {
+
+                                        tv.setText("" + remainingGuesses);
+
+
+                                    }
+                                    else {
+
+                                        statusdisplay.setText("You lost: the word was "+word);
+                                        marmot.setImageResource(R.drawable.mussolini);
+                                        mp2.start();
+                                        input.setEnabled(false);
+
+                                    }
+
+                                }
+                                else {
+                                    String actualVisible = "";
+                                    for(int i = 0; i < visible.length(); i+=2) {
+                                        actualVisible += visible.charAt(i);
+                                    }
+
+                                    if(actualVisible.equals(word)) {
+                                        statusdisplay.setText("Congratulations, you have won!");
+                                        marmot.setImageResource(R.drawable.teddy);
+                                        mp.start();
+                                        input.setEnabled(false);
+                                    }
+                                }
+
+                            }
+                            else {
+                                System.out.println("Invalid input!");
+                            }
+
+                            input.setText("");
+
+                        }
+        });
+
+    }
+
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
